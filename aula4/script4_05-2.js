76fbbd6a94fcd7667413b8433811ccf121bc8f0e
Hebert <hebertphp@gmail.com>
2022-04-08 20:25:54 -0300

    PFE_6a_N

---
+++ aula4/script4_05-2.js
@@ -0,0 +1,20 @@
+var x;
+x=10;
+x++;//11
+x++;//12
+++x;//13
+++x;//14
+document.write(x);//14
+x--;//13
+x--;//12
+--x;//11
+document.write("<br>"+x);//11
+
+/*
+x++, soma 1
+x=x+1, soma 1
+x+=1, soma 1
+
+x+=2, soma 2
+
+*/
