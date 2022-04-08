15a52340c378a818f8913a2e99e81c0694c10285
Hebert <hebertphp@gmail.com>
2022-04-08 20:10:39 -0300

    PFE_6a_N

---
+++ aula4/script4_05-1.js
@@ -0,0 +1,14 @@
+var a;
+a=prompt("Digite um número");//2021
+a=parseInt(a);
+document.write ("a ="+ (a++));//a =2021
+document.write ("<br>a ="+ a);//a =2022
+/*
+incremento, ++, somar 1
+decremento, --, subtrair 1
+
+pré-incremento, ++a, 
+pós-incremento, a++, 
+pré-decremento, --a, 
+pós-decremento, a--, 
+*/
