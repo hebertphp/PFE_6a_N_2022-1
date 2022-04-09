93dd0f8d0a86f3be4f343f0f9ecc850e4dd8c476
Hebert <hebertphp@gmail.com>
2022-04-08 21:00:56 -0300

    PFE_6a_N

---
+++ aula4/script4_09-1.js
@@ -0,0 +1,14 @@
+//igualdade
+var a,b,res;
+a=10;//   10 int
+b="10";// 10 string
+res=(a!==b);//res= true
+document.write("res = "+res);
+/*
+=    , recebe ou atribui
+==   , igualdade, checa apenas o valor
+===  , identicos, verifica o valor e o tipo
+!=   , não igual, diferentes
+!==  , não identico
+
+*/
---
+++ aula4/script4_13-1.js
@@ -0,0 +1,14 @@
+//menor, menor ou igual
+var a,b,c,d;
+a=10;//10 int
+b=10;//10 int
+c=(a<b);//c=false
+d=(a<=b);//d=true
+document.write("c = "+c);
+document.write("<br>d = "+d);
+/*
+<   menor
+<=  menor ou igual
+>   maior
+>=  maior ou igual
+*/
