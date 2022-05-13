37df09e714878e1b3906124ef9cace86867a66ba
Hebert <hebertphp@gmail.com>
2022-05-13 20:48:32 -0300

    PFE_6a_N

---
+++ aula5/script5_08-1.js
@@ -0,0 +1,47 @@
+var i;
+i=0; //valor inicial
+while (i <=10) {//condição
+    document.write(i + " ");
+    //i++;//contador
+    //i++;//i=i+1//i+=1
+    i+=2;
+}
+/*
+Saída:
+0 1 2 3 4 
+1) Qual o valor inicial? 0
+2) Qual a condição? i<5
+3) Qual o contador? i++
+4) Quantas vezes o looping foi executado? 5
+5) Qual o valor que deixou a condição como falsa? 5
+
+Desafio 1)
+Saída:
+7 8 9 10
+
+1) Qual o valor inicial? 7
+2) Qual a condição? i<11 ou i<=10
+3) Qual o contador? i++
+4) Quantas vezes o looping foi executado? 4
+5) Qual o valor que deixou a condição como falsa? 11
+
+Desafio 2)
+Saída:
+4 3 2 1
+
+1) Qual o valor inicial? 4
+2) Qual a condição? i >=1 ou i>0 
+3) Qual o contador? i--
+4) Quantas vezes o looping foi executado? 4
+5) Qual o valor que deixou a condição como falsa? 0
+
+Desafio 3)
+Saída:
+0 2 4 6 8 10
+
+1) Qual o valor inicial? 0
+2) Qual a condição? i<=10
+3) Qual o contador? i+=2;
+4) Quantas vezes o looping foi executado? 6
+5) Qual o valor que deixou a condição como falsa?
+*/
