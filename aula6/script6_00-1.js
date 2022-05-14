402d280561d72f7f4e2d86f8e21854c33f8c2f6e
Hebert <hebertphp@gmail.com>
2022-05-13 22:04:48 -0300

    PFE_6a_N

--- aula6/script6_00-1.js
+++ aula6/script6_00-1.js
@@ -4,8 +4,9 @@ n="Belarmina";
 n="Astrogildo";
 n="Pafuncia";
 //document.write(n);
+//        0              1         2           3
 nome=["Adalberto","Belarmina","Astrogildo","Pafuncia"];
 //document.write(nome);
 document.getElementById("dv1").innerHTML=n;
-document.querySelector(".teste").innerHTML=nome;
+document.querySelector(".teste").innerHTML=nome[0];
 document.querySelector("h2").innerHTML=n;
