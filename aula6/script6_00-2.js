5288d5eea1928945d2f03e1c5f16b9ff8f2ca8b3
Hebert <hebertphp@gmail.com>
2022-05-20 20:29:55 -0300

    PFE_6a_N

---
+++ aula6/script6_00-2.js
@@ -0,0 +1,15 @@
+var i,cidade;
+i=10;
+i=11;
+i=12;
+i=13;
+//          0           1          2          3  
+cidade=["São Paulo","Pedreira","São Pedro","Guarulhos"];
+document.querySelector("#dv1").innerHTML=cidade[2];
+document.querySelector(".teste").innerHTML=cidade[1];
+document.querySelector("h2").innerHTML=cidade[3];
+/*
+( )
+{ }
+[ ]
+*/
