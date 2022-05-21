4f37956c41ce2c21fa0a39b2fd6b221e857839c6
Hebert <hebertphp@gmail.com>
2022-05-20 21:47:55 -0300

    PFE_6a_N

--- aula6/script6_03-2.js
+++ aula6/script6_03-2.js
@@ -1,11 +1,11 @@
 var semana = ["DOMINGO", "SEGUNDA", "TERÇA", "QUARTA", "QUINTA","SEXTA", "SÁBADO"];
 for (var i = 0; i < 7; i++){
  //document.write(semana[i] + " é o " + (i+1) + "o dia da semana!<br>");
-
+    document.querySelector(".teste").innerHTML+=semana[i] + " é o " + (i+1) + "o dia da semana!<br>";
 }
 /*
 substituir o document.write, por queryselector direcionando 
 para a área amarela o conteúdo:
-semana[i] + " é o " + (i+1) + "o dia da semana!<br>
+semana[i] + " é o " + (i+1) + "o dia da semana!<br>"
 
 */
