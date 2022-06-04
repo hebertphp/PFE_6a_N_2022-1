9ca980887e5499a22cd6eb47a797a2a78fad75f4
Hebert <hebertphp@gmail.com>
2022-06-03 21:52:16 -0300

    PFE_6a_N

---
+++ aula6/script6_12.js
@@ -0,0 +1,19 @@
+const id_dv=document.querySelector("#dv");
+const class_teste=document.querySelector(".teste");
+const id_login=document.querySelector("#login");
+const tag_h2=document.querySelector("h2");
+
+//       0  1  2   3  4
+var x = [10,20,30,40,50,60,70,80];
+
+//      0 0 1 2       1 0 1 2       2 0 1 2  
+var m = [[1,2,3]   ,   [4,5,6]   ,   [7,8,9],[50,70,80,90],["SP","RJ"],[100,200,300,[400,500]]];
+
+id_dv.innerHTML=x.length;
+
+class_teste.innerHTML=m[5][3][1];
+/*
+mostrar os dois comandos para mostrar o valor 4,
+baseados na var m, direcionar para id_login
+
+*/
