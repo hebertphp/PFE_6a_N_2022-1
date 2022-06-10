1c5a97f68cb3b30c413c9e56d3b814bc80b2d0bf
Hebert <hebertphp@gmail.com>
2022-06-10 20:13:27 -0300

    PFE_6a_N

---
+++ aula6/revisa1.js
@@ -0,0 +1,12 @@
+const id_dv=document.querySelector("#dv");
+const class_teste=document.querySelector(".teste");
+const id_login=document.querySelector("#login");
+const tag_h2=document.querySelector("h2");
+//     0  1  2  3  4   5
+var x=[20,40,70,90,97,88];
+
+//       0      1      2              3
+var y=[[3,4],[5,6,7],[8,9],["oi","blz","pdp","haaa"]];
+//      0 1   0 1 2   0 1    0     1     2     3 
+id_dv.innerHTML=x[3];
+class_teste.innerHTML=y[3][1];
