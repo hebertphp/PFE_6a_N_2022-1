e27192f8b68115ad25c17e033c3b6f2aa280330b
Hebert <hebertphp@gmail.com>
2022-06-24 20:31:36 -0300

    PFE_6a_N

--- aula7/script7_01.js
+++ aula7/script7_01.js
@@ -7,7 +7,8 @@ function mediaf(av1,av2){
 
 mostrar("Epaminondas");//executar, chamar
 document.write("<br>");
-var n1=parseFloat(prompt("Digite a nota 1"));
-var n2=parseFloat(prompt("Digite a nota 2"));
-var x=mediaf(n1,n2);
+//var n1=parseFloat(prompt("Digite a nota 1"));
+//var n2=parseFloat(prompt("Digite a nota 2"));
+var x=mediaf(8,9);
 document.write("Media: "+x);
+document.write("<br>Outra Media:"+mediaf(6,9));
